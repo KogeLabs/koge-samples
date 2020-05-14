2a92f5585a3087e919ccd06caf089a1989c60763
koge <moncef.yabi@gmai.com>
2020-05-14 18:26:31 +0200

    Update source to Koge 0.1.1.3

--- src/main/kotlin/org/koge/samples/app.kt
+++ src/main/kotlin/org/koge/samples/app.kt
@@ -1,71 +1,349 @@
 package org.koge.samples
 
-import org.koge.engine.Game
-import org.koge.engine.event.key.Key
-import org.koge.engine.event.key.KeyDownEvent
-import org.koge.engine.event.mouse.*
-import org.koge.engine.graphics.Graphics
-import org.koge.game.sprite.Sprite
-import kotlin.math.sin
 
-class MyGame(width: Int, height: Int, title: String) : Game(width, height, title) {
+import org.jbox2d.callbacks.ContactImpulse
+import org.jbox2d.callbacks.ContactListener
+import org.jbox2d.collision.Manifold
+import org.jbox2d.collision.shapes.PolygonShape
+import org.jbox2d.common.Vec2
+import org.jbox2d.dynamics.*
+import org.jbox2d.dynamics.contacts.Contact
 
-    private var sprite = Sprite("/textures/boss.gif")
-    var xMouse=0.0
-    var yMouse=0.0
-    override fun init() {
-        sprite.init()
-        font.init(java.awt.Font(java.awt.Font.MONOSPACED, java.awt.Font.PLAIN, 16), true)
-        debugMode=true
-    }
+import org.koge.engine.audio.AudioPlayer
+import org.koge.engine.audio.Source
+import org.koge.engine.event.key.Keys
+import org.koge.engine.kernel.game
+import org.koge.engine.utils.PPM
+import org.koge.game.scene.Level
+import org.koge.game.scene.scene
+import org.koge.game.sprite.animatedsprite
+import org.koge.game.tilemap.ObjectLayer
+import java.awt.Font
 
-    override fun run(fps: Int) {
-        sprite.moveX(0.5f)
-        sprite.moveY(sin(sprite.position.x) *10)
-    }
 
-    override fun render(g: Graphics) {
-        g.draw(sprite)
-        g.drawText("Dummy text",10f,52f)
-    }
 
-    override fun destroy() {
-        sprite.destroy()
-    }
+//Screen width and height
+const val WIDTH = 500
+const val HEIGHT = 256
 
-    override fun keyDown(e: KeyDownEvent) {
-        when(e.key){
-            Key.KEY_UP -> sprite.moveY(-5f)
-            Key.KEY_DOWN -> sprite.moveY(5f)
-            Key.KEY_LEFT -> sprite.moveX(-5f)
-            Key.KEY_RIGHT -> sprite.moveX(5f)
+val worldG = World(Vec2(0f, 10f))
+
+
+val bodies= mutableListOf<Body>()
+fun addLevelBodies(width: Float, height:Float, xPos: Float, yPos:Float):Body{
+
+    val fd = FixtureDef().apply {
+        shape = PolygonShape().apply {
+            setAsBox(width, height)
         }
     }
 
-    override fun mouseButtonPressed(e: MousePressedEvent) {
-        println(e.button)
+    val bd = BodyDef().apply {
+        position.set(xPos, yPos)
+        type=BodyType.STATIC
     }
 
-    override fun mouseButtonReleased(e: MouseReleasedEvent) {
-        println(e.button)
-    }
-
-    override fun mouseCursorEnter(e: MouseCursorEnterEvent) {
-        println(e.entered)
-    }
-
-    override fun mouseMoved(e: MouseMovedEvent) {
-        xMouse = e.xpos
-        yMouse = e.ypos
-    }
-
-    override fun mouseScrolled(e: MouseScrollEvent) {
-        println("${e.xoffset} ${e.yoffset}")
+    return worldG.createBody(bd).apply {
+        createFixture(fd)
     }
 }
 
-fun main(){
-    MyGame(800, 600, "Koge Game").apply {
-        start()
+const val timeStep = 1.0f / 60f
+const val velocityIterations = 6
+const val positionIterations = 2
+
+
+val mario= animatedsprite(8,8){
+    texturePath="/textures/mario.png"
+    xPos=150f
+    yPos=230f
+    delay=5
+    setupPhysicsBody(
+        BodyDef().apply {
+            position.set(xPos/ PPM, yPos/PPM)
+            type = BodyType.DYNAMIC
+        },
+        FixtureDef().apply {
+            shape = PolygonShape().apply {
+                setAsBox(16/2.5f/PPM, 16/2.2f/PPM)
+            }
+            density = 0.9f
+            friction = 0.0f
+            restitution = 0.0f
+        }
+    )
+    frame {
+        name="stop_right"
+        count=1
+        loop=false
     }
+    frame {
+        name="run_right"
+        count=4
+        loop=true
+    }
+    frame {
+        name="jump_right"
+        count=2
+        loop=false
+    }
+    frame {
+        name="die_right"
+        count=1
+        loop=false
+    }
+    frame {
+        name="die_left"
+        count=1
+        loop=false
+    }
+    frame {
+        name="jump_left"
+        count=2
+        loop=false
+    }
+    frame {
+        name="run_left"
+        count=4
+        loop=true
+    }
+    frame {
+        name="stop_left"
+        count=1
+        loop=false
+    }
+
+    frame {
+        name="stop_right_big"
+        count=1
+        loop=false
+    }
+    frame {
+        name="run_right_big"
+        count=5
+        loop=true
+    }
+    frame {
+        name="die_right_big"
+        count=2
+        loop=false
+    }
+    frame {
+        name="jump_right_big"
+        count=2
+        loop=false
+    }
+
+    frame {
+        name="die_left_big"
+        count=2
+        loop=false
+    }
+    frame {
+        name="run_left_big"
+        count=5
+        loop=true
+    }
+    frame {
+        name="stop_left_big"
+        count=1
+        loop=false
+    }
+    frame {
+        name="jump_left_big"
+        count=2
+        loop=false
+    }
+    activeAnimationName="stop_right"
 }
+
+val enemy= animatedsprite(8,8){
+    texturePath="/textures/enemy.png"
+    xPos=800f
+    yPos=690f
+    delay=5
+
+    frame {
+        name="orange"
+        count=2
+        loop=true
+    }
+    frame {
+        name="orange_die"
+        count=1
+        loop=false
+    }
+
+    activeAnimationName="orange"
+
+}
+
+val scene1 = scene("Level 1") {
+
+    +mario
+    +enemy
+    var direction =-1f
+    var inTheAir=false
+
+    val level1= Level("/levels/mario_level.json")
+
+
+    whenInit {
+        level1.init()
+        world?.setContactListener(object: ContactListener{
+            override fun endContact(contact: Contact?) {
+
+            }
+
+            override fun beginContact(contact: Contact?) {
+                if(inTheAir){
+                    mario.physicsBody?.linearVelocity= Vec2(0f,0f)
+                    mario.physicsBody?.angularVelocity = 0f
+                    mario.setActiveAnimation("stop_right")
+                    mario.stopAnimation()
+                }
+                inTheAir=false
+
+            }
+
+            override fun preSolve(contact: Contact?, oldManifold: Manifold?) {
+
+            }
+
+            override fun postSolve(contact: Contact?, impulse: ContactImpulse?) {
+
+            }
+
+        })
+        var objectLayer = level1.tileMap?.layers?.get(1) as ObjectLayer
+        objectLayer.objects.forEach {lo->
+            bodies.add(
+                addLevelBodies(lo.width.toFloat()/2/PPM,
+                lo.height.toFloat()/2/PPM,
+                (lo.x.toFloat()+lo.width.toFloat()/2)/PPM,
+                (lo.y.toFloat()+lo.height.toFloat()/2)/ PPM )
+            )
+        }
+        objectLayer = level1.tileMap?.layers?.get(2) as ObjectLayer
+        objectLayer.objects.forEach {lo->
+            bodies.add(
+                addLevelBodies(lo.width.toFloat()/2/PPM,
+                lo.height.toFloat()/2/PPM,
+                (lo.x.toFloat()+lo.width.toFloat()/2)/PPM,
+                (lo.y.toFloat()+lo.height.toFloat()/2)/ PPM )
+            )
+        }
+        objectLayer = level1.tileMap?.layers?.get(3) as ObjectLayer
+        objectLayer.objects.forEach {lo->
+            bodies.add(
+                addLevelBodies(lo.width.toFloat()/2/PPM,
+                lo.height.toFloat()/2/PPM,
+                (lo.x.toFloat()+lo.width.toFloat()/2)/PPM,
+                (lo.y.toFloat()+lo.height.toFloat()/2)/ PPM )
+            )
+        }
+
+    }
+
+    whenKeyReleased {
+
+        if(!inTheAir){
+            mario.setActiveAnimation("stop_right")
+            mario.stopAnimation()
+            mario.physicsBody?.linearVelocity= Vec2(0f,0f)
+            mario.physicsBody?.angularVelocity = 0f
+
+        }
+    }
+    whenKeyPressed {
+
+        if (key.id==Keys.KEY_UP ){
+            if(!inTheAir){
+
+                mario.applyLinearImpulse(0f,-0.07f)
+                mario.setActiveAnimation("jump_right")
+                mario.startAnimation()
+                inTheAir=true
+            }
+        }
+        if (key.id==Keys.KEY_RIGHT && mario.physicsBody?.linearVelocity?.x!! <=1){
+            if(!inTheAir){
+                mario.setActiveAnimation("run_right")
+                mario.startAnimation()
+            }
+
+
+            mario.applyLinearImpulse(0.05f,0f)
+        }
+        if(key.id==Keys.KEY_LEFT && mario.physicsBody?.linearVelocity?.x!! >=-1){
+            if(!inTheAir){
+                mario.setActiveAnimation("run_left")
+                mario.startAnimation()
+            }
+            mario.applyLinearImpulse(-0.05f,0f)
+        }
+    }
+
+
+    whenUpdate {
+
+        if(enemy.position.x<=20f) direction =1f
+
+        else if(enemy.position.x>= 700) direction =-1f
+        enemy.moveX(direction*5f)
+
+        camera.setPosition(mario.position.x,0f)
+        camera.update()
+
+    }
+
+    render {
+        level1.sprites.forEach { sprite ->
+            g.draw(sprite)
+        }
+
+    }
+
+    whenDestroy {
+        level1.destroy()
+    }
+
+}
+
+
+fun main() {
+
+    lateinit var source: Source
+
+    game(WIDTH, HEIGHT, "Koge") {
+
+        +scene1
+
+        whenInit {
+            font.init(Font("Comic Sans MS", Font.PLAIN, 16), true)
+            setActiveScene("Level 1")
+
+            AudioPlayer.init()
+            AudioPlayer.setListenerData(0f, 0f, 0f)
+
+            val buffer: Int = AudioPlayer.loadSound("/audio/bg.wav")
+            source = Source()
+            source.setLooping(true)
+            source.play(buffer)
+            world = worldG
+        }
+
+        whenUpdate {
+            world?.step(timeStep, velocityIterations, positionIterations)
+        }
+
+        whenDestroy {
+            source.delete()
+            AudioPlayer.cleanUp()
+        }
+
+    }.start()
+
+
+}
+
